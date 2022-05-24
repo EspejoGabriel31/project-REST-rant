da7645233cb94d361389ce558ed6cd1ed398b7dd
unknown <gespejo2144@gmail.com>
2022-05-24 14:19:29 -0700

    adding remaining stub routes

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.get('.', (req, res) => {
+app.use('/places', require('./controllers/places'))
+
+app.get('/', (req, res) => {
     res.send('hello world')
 })
 
