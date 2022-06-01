b97135e5ba072b56da1a39705b484ada6c1e01df
unknown <gespejo2144@gmail.com>
2022-06-01 00:42:10 -0700

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -20,22 +20,23 @@ router.post('/', (req, res) => {
     places.push(req.body)
     res.redirect('/places')
   })
-  
-  router.get('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if(isNaN(id)){
-      res.render('error404')
-    }
-    else if(!place[id]){
-      res.render('error404')
-    }
-    else{
-      res.render('places/show', {place: places[id]})
-    }
-  })
 
 router.get('/', (req, res) => {
     res.render('places/index', { places })
+})
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if(isNaN(id)){
+    res.render('error404')
+  }
+  else if(!places[id]){
+    res.render('error404')
+  }
+  else{
+    res.render('places/show', { place: places[id] })
+
+  }
 })
 
 module.exports = router
