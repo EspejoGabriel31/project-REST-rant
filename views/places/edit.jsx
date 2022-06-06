9d9e5d43d62ae58433c8c01ccef6679567a08a3a
unknown <gespejo2144@gmail.com>
2022-06-05 17:40:29 -0700

    edit page adjustments

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -8,7 +8,8 @@ function edit_form({place, index}){
             <main>
                 <h1>Edit Place Information</h1>
                 <form method="POST" action={`/places/${index}?_method=PUT`}>
-                    <div className="form-group">
+                    <div className='row'>
+                        <div className="form-group col-sm-6">
                         <label htmlFor="name">Place Name</label>
                         <input className='form-control' 
                             id="name" 
@@ -16,33 +17,35 @@ function edit_form({place, index}){
                             defaultValue = {place.name}
                             required
                         />
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="pic">Place Picture</label>
+                            <input className='form-control'  
+                                id="pic"
+                                name="pic"
+                                defaultValue={place.pic}
+                            />
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="pic">Place Picture</label>
-                        <input className='form-control' 
-                            type="url" 
-                            id="pic"
-                            name="pic"
-                            defaultValue={place.pic}
-                        />
+                    <div className='row'>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="city">City</label>
+                            <input className='form-control' 
+                                id="city" 
+                                name="city"
+                                defaultValue={place.city}
+                            />
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="state">State</label>
+                            <input className='form-control' 
+                                id="state" 
+                                name="state"
+                                defaultValue={place.state}
+                            />
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="city">City</label>
-                        <input className='form-control' 
-                            id="city" 
-                            name="city"
-                            defaultValue={place.city}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="state">State</label>
-                        <input className='form-control' 
-                            id="state" 
-                            name="state"
-                            defaultValue={place.state}
-                        />
-                    </div>
-                    <div className="form-group">
+                    <div className="form-group col-sm-12">
                         <label htmlFor="">Cuisines</label>
                         <input className='form-control' 
                             id="cuisines" 
